d8ef12796c7dd6f2fb49a86d833ebd0a4ef8cc64
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-14 15:09:21 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -15,7 +15,7 @@ async def on_message(message):
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
-    print('Bot is ready.')
+    print('Ready.')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
