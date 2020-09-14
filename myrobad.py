92688cb56f629db289bd2d97bfbbcc063d547d1d
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-14 14:50:48 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -3,9 +3,13 @@ import os
 
 client = discord.Client()
 
+
+# --------------- STATUS -------------------#
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name="myro the best", type = 3))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="myro the best")
     print('On')
+# -------------------------------------------#
+
 
 client.run(os.environ['DISCORD_TOKEN'])
