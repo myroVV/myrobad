01bf16baa558d94cd91140883671e3cb0f1b7c84
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-14 16:13:55 +0800

    React2

--- myrobad.py
+++ myrobad.py
@@ -6,16 +6,19 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix='#')
 
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
+    print('myrosaurus is ready.')
+
+
+
 @bot.event
 async def on_message(message):
     if(message.channel.id == "698639022125219903"):
         await bot.add_reaction(message, ":thinking_face:")
 
 
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
-    print('myrosaurus is ready.')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
