553aa4343996b3096ec415a9d631873259b76dd6
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-14 15:06:04 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -1,12 +1,27 @@
 import discord
 import os
+from discord.ext import commands
 
 client = discord.Client()
+
+bot = commands.Bot(command_prefix='#')
+
+@bot.event
+async def on_message(message):
+    if(message.channel.id == "698639022125219903"):
+        await bot.add_reaction(message, ":thinking:")
+
 
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
     print('Bot is ready.')
+
+
+@client.event
+async def on_message(message):
+    if message.content.lower().strip() == 'no one asked'
+    await message.content.send('I care')
 
 
 client.run(os.environ['DISCORD_TOKEN'])
