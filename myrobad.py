40e9df915296ea4e3178d229d113c4f4e83876ba
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-15 10:17:35 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -1,13 +1,9 @@
-# Standard libraries
-import os
-import logging
-
-# Third party libraries
 import discord
+import os
 from discord.ext import commands
 
 
-@client.event
+@bot.event
 async def on_message(message):
     if message.channel.id == 698639022125219903:
         await message.add_reaction("😀")
@@ -30,7 +26,7 @@ async def on_message(message):
 
 
 
-@client.event
+@bot.event
 async def on_message(message):
     if message.channel.id == 738971995068170240:
         await message.add_reaction("😀")
@@ -53,4 +49,4 @@ async def on_message(message):
 
     
 
-    client.run(os.environ['DISCORD_TOKEN'])
+client.run(os.environ['DISCORD_TOKEN'])
