53d35ca6f4bf20a2026c1a793592f12b514f7b2f
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-15 10:23:05 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -4,6 +4,12 @@ from discord.ext import commands
 
 
 @bot.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
+    print ('Bot is online')
+
+
+@bot.event
 async def on_message(message):
     if message.channel.id == 698639022125219903:
         await message.add_reaction("😀")
