2f0c905a1307746233aaa2d605fa570d2b82fd29
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-16 10:42:19 +0800

    wqewq

--- myrobad.py
+++ myrobad.py
@@ -9,16 +9,11 @@ async def on_ready():
     await client.change_presence(status=discord.Status.dnd, activity=discord.Game('myro > all'))
     print ('Online')
 
-@client.event
-async def on_message(message):
-    if message.channel.id == 698639022125219903:
-        await message.channel.send("no one asked")
 
 @client.event
 async def on_message(message):
     if message.author.id == 701974976911245382:
         return
-
 
 
 client.run(os.environ['DISCORD_TOKEN'])
