d40959f555b61aae9f2996aa511db41646fa9ad7
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-16 11:51:49 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -1,7 +1,7 @@
 import discord
-import os
 from discord.ext import commands
 import asyncio
+import os
 
 client = discord.Client()
 
@@ -17,14 +17,17 @@ async def on_message(message):
     if message.author.id == 701974976911245382:
         return
 
-@client.event
-async def on_message(message):
-    if message.channel.id == 698639022125219903:
-        await message.add_reaction('🔴')
-        await message.add_reaction('🟠')
-        await message.add_reaction('🟡')
-        await message.add_reaction('🟢')
-        await message.add_reaction('🔵')
-        await message.add_reaction('🟣')
+async def mute(ctx, user : discord.Member, duration = 0,*, unit = None):
+    roleobject = discord.utils.get(ctx.message.guild.roles, id=730016083871793163)
+    await ctx.send(f":white_check_mark: Muted {user} for {duration}{unit}")
+    await user.add_roles(roleobject)
+    if unit == "s":
+        wait = 1 * duration
+        await asyncio.sleep(wait)
+    elif unit == "m":
+        wait = 60 * duration
+        await asyncio.sleep(wait)
+    await user.remove_roles(roleobject)
+    await ctx.send(f":white_check_mark: {user} was unmuted")    
 
 client.run(os.environ['DISCORD_TOKEN'])
