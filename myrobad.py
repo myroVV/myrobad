26ebf36e487868ebaadc8455405bde9d49b5d6a3
myroVV <71244566+myroVV@users.noreply.github.com>
2020-09-18 13:15:44 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -10,4 +10,10 @@ async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('myro > all'))
     print('Bot is online')
 
+
+@client.event
+async def on_message():
+    if message.channel.id == 756327903679217695:
+        await message.add_reaction("😱")
+
 client.run(os.environ['DISCORD_TOKEN'])
