da35b9781b0948bded04c2bc7e35c2c01e8675e4
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-03 22:34:14 +0800

    qwe

--- myrobad.py
+++ myrobad.py
@@ -2,12 +2,17 @@
 import discord
 from discord.ext import commands
 import os
+import pyautogui, time
+
+f = open("despise, 'r'")
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('myro > all'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('I despise luna'))
     print('Bot is online')
 
 @client.event
@@ -15,6 +20,8 @@ async def on_message(message):
 
      if "no one asked" in message.content:
            await Bot.send_message(message.channel, 'I asked wdym u mean')
+
+           
 
 
 client.run(os.environ['DISCORD_TOKEN'])
