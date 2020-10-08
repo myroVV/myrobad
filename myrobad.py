c3e7da5d30e290b07b3b231105127f7432c1ce11
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-08 08:36:10 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -10,7 +10,4 @@ async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('𝟗 𝟗 𝟗'))
     print('Bot is online')
 
-@client.event
-async def on_message(message):
-
 client.run(os.environ['DISCORD_TOKEN'])
