8626b2f100a4c7d088cbb52fa28459fa167651d2
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-09 15:41:54 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -5,6 +5,11 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+<<<<<<< HEAD
 await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Whole Lotta Red"))
+=======
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="WRLD On Drugs"))
+    print('Bot is online')
+>>>>>>> parent of 9c645e4... Update myrobad.py
 
 client.run(os.environ['DISCORD_TOKEN'])
