d814dd34b7c48a91c1c4e52bb31bae0d712160be
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-09 15:39:41 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -1,15 +1,10 @@
-# Standard libraries
 import discord
-from discord.ext import commands
 import os
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="WRLD On Drugs"))
-    print('Bot is online')
-
-
+await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Whole Lotta Red"))
 
 client.run(os.environ['DISCORD_TOKEN'])
