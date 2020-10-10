e42ea17ac296b88420d3e792be8f6b5fea31141a
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 17:31:51 +0800

    qweqw

--- cogs/covid.py
+++ cogs/covid.py
@@ -6,16 +6,6 @@ import asyncio
 from aiohttp import ClientSession
 import aiohttp
 
-from discord.ext import commands
-
-class Test(commands.Cog):
-
-    def __init__(self, client):
-        self.client = client
-        
-
-
-
 
 
 
@@ -259,7 +249,5 @@ class Covid(commands.Cog):
 
 
 
-
-#NEED
 def setup(client):
-    client.add_cog(Test(client))
+    client.add_cog(Covid(client))
