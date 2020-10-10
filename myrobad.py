b3bc33fee509898a6c422cae29cff3a3a3cafb65
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 21:50:00 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -324,18 +324,19 @@ async def unmute(ctx, member: discord.Member):
 @client.command()
 async def help(ctx):
     embed = discord.Embed(title="Commands", description="More commands coming soon!", color=0x6da860, timestamp=datetime.utcnow())
-    embed.add_field(name="Fun", value=f"``.help fun``", inline=False)
-    embed.add_field(name="Mod", value=f"``.help Mod``", inline=False)
-    embed.add_field(name="Info", value=f"``.help Info``", inline=False)
+    embed.add_field(name="._8ball (Question)", value=f"8ball O_O.", inline=False)
+    embed.add_field(name=".pp (User)", value=f"Find out how long your peepee is!", inline=False)
+    embed.add_field(name=".coinflip, .cf", value=f"flips a coin!", inline=False)
+    embed.add_field(name=".Ban", value=f"Bans a user.", inline=False)
+    embed.add_field(name=".Unban", value=f"Unbans a user.", inline=False)
+    embed.add_field(name=".Kick", value=f"Kicks a user.", inline=False)
+    embed.add_field(name=".userinfo (User)", value=f"Shows the info of the given user.", inline=False)
+    embed.add_field(name=".Covid (Country/Continent) (Name)", value=f"Shows the COVID statistics.", inline=False)
+    embed.add_field(name=".Ping", value=f"Gets the latency of the bot.", inline=False)
     embed.set_footer(text=f"Suggested by: {ctx.author}", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
 
 
-
-@client.command()
-async def help_fun(self, ctx):
-    embed = discord.Embed(title="Commands", description="More commands coming soon!", color=0x6da860, timestamp=datetime.utcnow())
-    embed.add_field(name="Fun", value=f"``.help fun``", inline=False)
 
 
 
