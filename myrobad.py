8686e10ae9fb1eb6184598c026f4855597065821
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 13:44:55 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -50,22 +50,6 @@ async def ban(ctx, member:discord.Member, *, reason=None):
 
 
 
-@client.command()
-@commands.has_permissions(administrator=True)
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#') 
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if(user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f"Unbanned{user.mention}"
- 
-
-
-
 
 
 client.run(os.environ['DISCORD_TOKEN'])
