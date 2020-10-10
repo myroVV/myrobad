03109d20c7e70838eb85c81befae2b1eeb6c752a
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 17:20:38 +0800

    asdqw

--- myrobad.py
+++ myrobad.py
@@ -26,6 +26,12 @@ async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+
+
 
 @client.event
 async def on_ready():
