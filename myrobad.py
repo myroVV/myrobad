466b9504101d1ae990d421ab453ab8d2ddf0d585
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 14:01:39 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -91,5 +91,42 @@ async def members(ctx):
 
 
 
+@bot.command(pass_context=True)
+async def hack(ctx, member:discord.Member = None):
+    if not member:
+        await ctx.send("Please specify a member")
+        return
+
+    passwords=['imnothackedlmao','sendnoodles63','ilovenoodles','icantcode','christianmicraft','server','icantspell','hackedlmao','WOWTONIGHT','69'] 
+    fakeips=['154.2345.24.743','255.255. 255.0','356.653.56','101.12.8.6053','255.255. 255.0']
+
+    embed=discord.Embed(title=f"**Hacking: {member}** 0%", color=0x2f3136)
+    m = await ctx.send(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 19%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 34%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 55%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 67%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 84%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 99%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(1)
+    embed=discord.Embed(title=f"**Hacking: {member}** 100%", color=0x2f3136)
+    await m.edit(embed=embed)
+    time.sleep(3)
+    embed=discord.Embed(title=f"{member} info ", description=f"*Email `{member}@hacked.com` Password `{random.choice(passwords)}`  IP `{random.choice(fakeips)}`*", color=0x2f3136)
+    embed.set_footer(text="this is a joke plses dont worry.")
+    await m.edit(embed=embed)
+    time.sleep(1)
 
 client.run(os.environ['DISCORD_TOKEN'])
