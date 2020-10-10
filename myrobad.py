50e6da3f6070d4fc0aa23bff4a562f2b5c99dc81
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 17:04:14 +0800

    Update myrobad.py

--- myrobad.py
+++ myrobad.py
@@ -259,7 +259,7 @@ async def boop (ctx, member:discord.User=None):
     if member == None or member == ctx.message.author:
         await ctx.channel.send("``You cannot boop yourself``")
         return
-    await ctx.channel.send(f"``✅ {member} **Has been successfully booped! ✅**")
+    await ctx.channel.send(f"``✅ {member} **Has been successfully booped! ✅**``")
 
 
       
