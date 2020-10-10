6bf6c7b23aaa4b4f29bf2fc2b784f63c45aa4704
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 18:08:03 +0800

    ?

--- cogs/covid.py
+++ cogs/covid.py
@@ -249,8 +249,5 @@ class Covid(commands.Cog):
 
 
 
-
-
-
 def setup(client):
     client.add_cog(Covid(client))
--- myrobad.py
+++ myrobad.py
@@ -265,24 +265,7 @@ async def boop (ctx, member:discord.User=None):
     await ctx.channel.send(f"``✅ {member} Has been successfully booped! ✅``")
 
 
-      
-
-@client.command(aliases=['userinfo'])
-async def userinfo(ctx, member: discord.Member):
-    roles = [role for role in member.roles]
-
-    embed=discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at)
-    embed.set_author(name=f"User Info - {member}")
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f"Requested By {ctx.author}", icon_url=ctx.author.avatar_url)
-    embed.add_field(name="ID:", value=member.id)
-    embed.add_field(name="Member Name:", value=member.display_name)
-    embed.add_field(name="Created:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-    embed.add_field(name="Join Date:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-    embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
-    embed.add_field(name="Top Role:", value=member.top_role.mention)
-    embed.add_field(name="Bot?", value=member.bot)
-    await ctx.send(embed=embed)
+    
 
 
 
