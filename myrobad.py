fa23b14ec8cb789eb25fbf7dfb5fa817fbd40b66
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 17:08:51 +0800

    ddddddddddddddddddd

--- myrobad.py
+++ myrobad.py
@@ -19,7 +19,11 @@ client = commands.Bot(command_prefix=".")
 
 @client.command()
 async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
 
 
