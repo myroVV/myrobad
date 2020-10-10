3d5ba10815511ebd9c165c43e3531779228b550c
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-10 20:54:22 +0800

    weqqqqqqqwqeqwwqeqweqweqwewqewqe

--- myrobad.py
+++ myrobad.py
@@ -18,25 +18,6 @@ client = discord.Client()
 client = commands.Bot(command_prefix=".")
 client.remove_command("help")
 
-import json
-import os
-
-if os.path.exists(os.getcwd() + "/config.json"):
-    
-    with open("./config.json") as f:
-        configData = json.load(f)
-
-else:
-    configTemplate = {"Token": "", "Prefix": "."}
-
-    with open(os.getcwd() + "/config.json", "w+") as f:
-        json.dump(configTemplate, f) 
-
-token = configData["Token"]
-prefix = configData["Prefix"]
-
-
-
 
 
 @client.command()
