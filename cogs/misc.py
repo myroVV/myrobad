5579d66326c0cf5b2320cd50f70699d6dcf0332c
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-11 11:06:29 +0800

    wq

--- cogs/misc.py
+++ cogs/misc.py
@@ -32,24 +32,6 @@ class Misc(commands.Cog):
 
 
 
-    @commands.command(aliases=['uinfo'])
-    async def userinfo(self, ctx, member: discord.Member):
-        roles = [role for role in member.roles]
-
-        embed=discord.Embed(color=ctx.author.color, timestamp=ctx.message.created_at)
-        embed.set_author(name=f"User Info - {member}")
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(text=f"Requested By {ctx.author}", icon_url=ctx.author.avatar_url)
-        embed.add_field(name="ID:", value=member.id)
-        embed.add_field(name="Member Name:", value=member.display_name)
-        embed.add_field(name="Created:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-        embed.add_field(name="Joined Server:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-        embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
-        embed.add_field(name="Top Role:", value=member.top_role.mention)
-        embed.add_field(name="Bot", value=member.bot)
-        await ctx.send(embed=embed)
-
-
 
 def setup(client):
     client.add_cog(Misc(client))
---
+++ moderation.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+    """
+    modsZ
+    """
+
+
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Mod Cog has been loaded\n-----")
+
+
+
+@commands.command(pass_context=True, aliases=['uinfo'])
+async def userinfo2(ctx, user: discord.Member=None):
+    user = user or ctx.message.author
+    embed = discord.Embed(title=f"{user}'s info", color=0xffffff)
+    embed.add_field(name="User info:", value=f'Username: **{user.name + "#" + user.discriminator}**\nID: **{user.id}**\nRegistered: **{user.created_at.strftime("%a, %d %b %Y %I:%M %p")}**\nStats: {user.status}\nBot: **{user.bot}**', inline=False)
+    embed.add_field(name="Member info", value=f'Nickname: **{user.nick}**\nJoined date: **{user.joined_at.strftime("%a, %d %b %Y %I:%M %p")}**\nHighest Role: **{user.top_role}**', inline=False)                
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
+    await ctx.send(embed=embed, delete_after=10.5)
+
+
+
+def setup(client):
+    client.add_cog(Moderation(client))
