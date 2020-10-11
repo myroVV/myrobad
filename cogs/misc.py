978f355e87397da7d75870d3cb581c09a696b23f
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-11 15:13:11 +0800

    hjioop

--- cogs/misc.py
+++ cogs/misc.py
@@ -34,32 +34,6 @@ class Misc(commands.Cog):
 
 
 
-    @commands.command(aliases=['lock'])
-    @commands.has_guild_permissions(manage_channels=True)
-    @commands.bot_has_guild_permissions(manage_channels=True)
-    async def lockdown(self, ctx, channel: discord.TextChannel=None):
-            channel = channel or ctx.channel
-
-            if ctx.guild.default_role not in channel.overwrites:
-                overwrites = {
-                ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False)
-                }
-                await channel.edit(overwrites=overwrites)
-                embed = discord.Embed(title="Channel locked", description="⛓️", color=0x6da860)
-                await ctx.send(embed=embed)
-            elif channel.overwrites[ctx.guild.default_role].send_messages == True or channel.overwrites[ctx.guild.default_role].send_messages == None:
-                overwrites = channel.overwrites[ctx.guild.default_role]
-                overwrites.send_messages = False
-                embed = discord.Embed(title="Channel locked", description="⛓️", color=0x6da860)
-                await ctx.send(embed=embed)
-            else:
-                overwrites = channel.overwrites[ctx.guild.default_role]
-                overwrites.send_messages = True
-                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrites)
-                embed = discord.Embed(title="Channel unlocked", description="⛓️", color=0x6da860)
-                await ctx.send(embed=embed)
-
-
 
 def setup(client):
     client.add_cog(Misc(client))
