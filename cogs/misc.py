f04f43f79b2f369f0e759ced0845143c20c286f5
myroVV <71244566+myroVV@users.noreply.github.com>
2020-10-11 10:25:05 +0800

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import requests
 from aiohttp import ClientSession
 
 class Misc(commands.Cog):
@@ -29,35 +30,18 @@ class Misc(commands.Cog):
                 embed = discord.Embed(title=f'***Random Fact***', colour=ctx.author.colour, timestamp=ctx.message.created_at)
 
                 embed.add_field(name='**Fun Fact**', value=fact, inline=False)
-                await ctx.send(embed=embed)
 
 
 
-
-    @commands.command()
-    async def insta(self, ctx, username):
-        url = f'https://apis.duncte123.me/insta/{username}'
-        async with ClientSession() as session:
-            async with session.get(url) as response:
-                r = await response.json()
-                data = r['user']
-                username = data["username"]
-                followers = data["followers"]["count"]
-                following = data["following"]["count"]
-                uploads = data["uploads"]["count"]
-                biography = data["biography"]
-                private = data["is_private"]
-                verified = data["is_verified"]
-
-                embed = discord.Embed(title=f'Insta Details: {username}')
-                embed.add_field(name='Bio', value=biography + '\u200b', inline=False)
-                embed.add_field(name='Private Status', value=private, inline=False)
-                embed.add_field(name='Verified Status', value=verified, inline=False)
-                embed.add_field(name='Followers', value=followers, inline=False)
-                embed.add_field(name='Following', value=following, inline=False)
-                embed.add_field(name='Posts', value=uploads, inline=False)
-                await ctx.send(embed=embed)
-
+    @commands.command(aliases=['Meme', "M"])
+    async def meme(self, msg:commands.Context):
+        fetch:discord.Message = await msg.channel.send("Getting a meme...")
+        req = requests.request("GET",'https://apis.duncte123.me/meme')
+        meme = req.json()
+        emb = discord.Embed()
+        emb.set_image(url=meme["data"]["image"])
+        emb.add_field(name="Quality meme", value=f'[{meme["data"]["title"]}]({meme["data"]["url"]})')
+        await fetch.edit(embed=emb)
 
 
 
